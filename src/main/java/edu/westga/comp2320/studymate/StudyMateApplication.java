a1abda17b92facf1e602a25d21ef8f5443c7b760
Raphael Rao <rrao1@my.westga.edu>
2026-04-16 18:55:21 -0400

    Build GUI layout in SceneBuilder

--- src/main/java/edu/westga/comp2320/studymate/StudyMateApplication.java
+++ src/main/java/edu/westga/comp2320/studymate/StudyMateApplication.java
@@ -11,8 +11,8 @@ public class StudyMateApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StudyMateApplication.class.getResource("studymate-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 300);
+        stage.setTitle("Study Organizer by Raphael Rao");
         stage.setScene(scene);
         stage.show();
     }
